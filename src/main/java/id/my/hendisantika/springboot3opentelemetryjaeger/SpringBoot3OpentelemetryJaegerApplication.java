dc8b6f9b24ea875ae2a9b76aeff547d8d0712e5d
hendisantika <hendisantika@yahoo.co.id>
2024-04-19 16:36:29 +0700

    Create starter project  🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springboot3opentelemetryjaeger/SpringBoot3OpentelemetryJaegerApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springboot3opentelemetryjaeger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3OpentelemetryJaegerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot3OpentelemetryJaegerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/springboot3opentelemetryjaeger/SpringBoot3OpentelemetryJaegerApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springboot3opentelemetryjaeger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3OpentelemetryJaegerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
