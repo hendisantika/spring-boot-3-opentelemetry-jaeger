7519d0e57ac050f840264e7d5d51e6fb9dad691d
hendisantika <hendisantika@yahoo.co.id>
2024-04-19 16:39:17 +0700

    Add DemoController 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springboot3opentelemetryjaeger/controller/DemoController.java
@@ -0,0 +1,19 @@
+package id.my.hendisantika.springboot3opentelemetryjaeger.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-3-opentelemetry-jaeger
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 4/19/24
+ * Time: 16:38
+ * To change this template use File | Settings | File Templates.
+ */
+@RestController
+@Slf4j
+public class DemoController {
+}
