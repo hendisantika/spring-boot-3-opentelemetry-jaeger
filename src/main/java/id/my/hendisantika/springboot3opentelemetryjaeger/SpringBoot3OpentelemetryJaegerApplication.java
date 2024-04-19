8ba25b1846bf466fa97c5f52542e73b518dbe56c
hendisantika <hendisantika@yahoo.co.id>
2024-04-19 16:38:16 +0700

    Add restTemplate Bean 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springboot3opentelemetryjaeger/SpringBoot3OpentelemetryJaegerApplication.java
+++ src/main/java/id/my/hendisantika/springboot3opentelemetryjaeger/SpringBoot3OpentelemetryJaegerApplication.java
@@ -2,12 +2,22 @@ package id.my.hendisantika.springboot3opentelemetryjaeger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@Configuration
 public class SpringBoot3OpentelemetryJaegerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBoot3OpentelemetryJaegerApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
     }
 
 }
