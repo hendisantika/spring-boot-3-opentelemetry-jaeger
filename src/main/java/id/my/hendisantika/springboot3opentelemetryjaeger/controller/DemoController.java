7a4ee5c699a09f3eca6a3f54ff219105accc47fa
hendisantika <hendisantika@yahoo.co.id>
2024-04-19 16:40:18 +0700

    Add path1 in DemoController 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springboot3opentelemetryjaeger/controller/DemoController.java
+++ src/main/java/id/my/hendisantika/springboot3opentelemetryjaeger/controller/DemoController.java
@@ -1,10 +1,14 @@
 package id.my.hendisantika.springboot3opentelemetryjaeger.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.time.LocalDateTime;
+import java.util.Random;
 
 /**
  * Created by IntelliJ IDEA.
@@ -19,8 +23,23 @@ import java.time.LocalDateTime;
 @RestController
 @Slf4j
 public class DemoController {
+    Random random = new Random();
+
+    private RestTemplate restTemplate;
+    @Value("${spring.application.name}")
+    private String applicationName;
+
     @GetMapping("/hello")
     public String index() {
         return "Greetings from Spring Boot! " + LocalDateTime.now();
+    }
+
+    @GetMapping("/path1")
+    public ResponseEntity<String> path1() {
+        log.info("Incoming request at {} for request /path1 ", applicationName);
+        doNothingButSleepForSomeTime();
+        String response = restTemplate.getForObject("http://localhost:8090/hello/path2", String.class);
+        doNothingButSleepForSomeTime();
+        return ResponseEntity.ok("response from /path1 + " + response);
     }
 }
